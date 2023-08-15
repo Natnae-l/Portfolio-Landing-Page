208b9e4f79c842fd655839bfdca402b7d66b1f41
Natna-el <130354087+Natna-el@users.noreply.github.com>
2023-08-15 06:27:48 -0700

    close tab added for mobile view

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 document.querySelector(".menu-bar").addEventListener("click", showMenu)
+document.querySelector(".close").addEventListener("click", returnMenu)
 
 function showMenu(){
     document.querySelector("nav").classList.toggle("show")
+    document.querySelector(".close").classList.toggle("show-menu")
+    document.querySelector(".menu-bar").classList.toggle("show-menu")
+}
+function returnMenu(){
+    document.querySelector("nav").classList.toggle("show")
+    document.querySelector(".close").classList.toggle("show-menu")
+    document.querySelector(".menu-bar").classList.toggle("show-menu")
 }
