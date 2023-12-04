66e8657e58f6aee3897622c54b396b7c8d98a3b8
Natnae-l <131717014+Natnae-l@users.noreply.github.com>
2023-12-04 20:24:22 +0300

    some paragraphs deleted

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-document.querySelector(".menu-bar").addEventListener("click", showMenu)
-document.querySelector(".close").addEventListener("click", returnMenu)
-document.querySelector(".go").addEventListener("click", returnMenu)
-document.querySelector(".go1").addEventListener("click", returnMenu)
-document.querySelector(".go2").addEventListener("click", returnMenu)
-document.querySelector(".go3").addEventListener("click", returnMenu)
+// document.querySelector(".menu-bar").addEventListener("click", showMenu)
+// document.querySelector(".close").addEventListener("click", returnMenu)
+// document.querySelector(".go").addEventListener("click", returnMenu)
+// document.querySelector(".go1").addEventListener("click", returnMenu)
+// document.querySelector(".go2").addEventListener("click", returnMenu)
+// document.querySelector(".go3").addEventListener("click", returnMenu)
 
-function showMenu(){
-    document.querySelector("nav").classList.toggle("show")
-    document.querySelector(".close").classList.toggle("show-menu")
-    document.querySelector(".menu-bar").classList.toggle("show-menu")
-}
+// function showMenu(){
+//     document.querySelector("nav").classList.toggle("show")
+//     document.querySelector(".close").classList.toggle("show-menu")
+//     document.querySelector(".menu-bar").classList.toggle("show-menu")
+// }
 
-function returnMenu(){
-    document.querySelector("nav").classList.toggle("show")
-    document.querySelector(".close").classList.toggle("show-menu")
-    document.querySelector(".menu-bar").classList.toggle("show-menu")
-}
+// function returnMenu(){
+//     document.querySelector("nav").classList.toggle("show")
+//     document.querySelector(".close").classList.toggle("show-menu")
+//     document.querySelector(".menu-bar").classList.toggle("show-menu")
+// }
+$(function(){
+	$('.box-menu .wrapper').on('click', function(){
+		$('.box-menu').toggleClass('full-menu');
+		$('.hamburger').toggleClass('active');
+        document.querySelector('header').style.minHeight = '100vh'
+	});
+	$('a').on('click', function(){
+		$(this).siblings('a').removeClass('active');
+		$(this).addClass('active');
+	});
+});
